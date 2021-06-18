70115804352c4afed1dd92ae8472f95dc40c9c1f
Sayumi <72898831+SayumiBunny@users.noreply.github.com>
2021-06-18 11:45:39 -0400

    Update defaultstuff.py

--- StuffsWeNeed/defaultstuff.py
+++ StuffsWeNeed/defaultstuff.py
@@ -1,1 +1,17 @@
+import json
 
+def config(filename: str = "config"):
+    try:
+        with open(f"StuffsWeNeed/jsons/{filename}.json", encoding='utf8') as data:
+            return json.load(data)
+    except FileNotFoundError:
+        raise FileNotFoundError("config.json wasn't found")
+
+def texts(filename: str = "texts"):
+    try:
+        with open(f"StuffsWeNeed/jsons/{filename}.json", encoding='utf8') as data:
+            return json.load(data)
+    except FileNotFoundError:
+        raise FileNotFoundError("texts.json wasn't found")
+
+# im going to cry now :( -Sayu
